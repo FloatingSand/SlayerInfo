e367b7d92ac662aae506dffa7ae56ab872ff211e
FloatingSand <thekillerofmc11roblox@gmail.com>
2024-06-19 19:32:20 -0400

    included new files

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import "./commands.js"
 import "./features/core.js"
 import "./features/effectsHud.js"
 // import "./features/utilitiesDisplay.js"
 import "./features/arrowDisplay.js"
+import "./newSetting.js"
+
