240f534852900357513cdd6685043dd9a54cb494
FloatingSand <thekillerofmc11roblox@gmail.com>
2024-06-19 23:53:52 -0400

    made some settings work

--- newSetting.js
+++ newSetting.js
@@ -2,7 +2,6 @@
 import Settings from "../Amaterasu/core/Settings"
 import DefaultConfig from "../Amaterasu/core/DefaultConfig"
 
-
 const CREDITS = FileLib.read("SlayerMod", "markdown/credits.md")
 
 const schemes = ["data/ColorScheme.json", "data/scheme-vigil.json", "data/scheme-nwjn.json"]
@@ -30,7 +29,7 @@ defaultConf
         description: "Copy API key to clipboard and press the PASTE button", 
         onClick(apiPaste) {
            let copied = Java.type("net.minecraft.client.gui.GuiScreen").func_146277_j()
-           ChatLib.command(`sm setapi ${copied}`,true)
+           ChatLib.command(`smapi setapi ${copied}`,true)
            ChatLib.chat(`&5&l[SlayerTracker]:&a API key set to &f${copied}!`)
         }
     })
@@ -67,7 +66,11 @@ defaultConf
         title: "Display Blaze Slayer Effects",
         description: "Renders a HUD for Smoldering Polarization and Wisp's Ice-flavored potion duration",
         value: false,
-        subcategory: "Effect Hud"
+        subcategory: "Effect Hud",
+        registerListener(previousvalue, newvalue) {
+            ChatLib.command("smeffecthud update",true)
+            
+        }
     })
     .addButton({
         category: "Displays", 
@@ -76,7 +79,7 @@ defaultConf
         description: "Click to move", 
         subcategory: "Effect Hud",
         onClick(setting) {
-          // todo
+            ChatLib.command("smeffecthud changegui", true)
         }
     })
 
@@ -86,7 +89,11 @@ defaultConf
         title: "Display Arrow Hud",
         description: "Renders a HUD that displays current arrows in quiver or in Inventory as well as the two types of poisons",
         value: false,
-        subcategory: "Arrow HUD"
+        subcategory: "Arrow HUD",
+        registerListener(previousvalue, newvalue) {
+            ChatLib.command("smarrowhud update",true)
+            
+        }
     })
     .addButton({
         category: "Displays", 
@@ -95,97 +102,12 @@ defaultConf
         description: "Click to move", 
         subcategory: "Arrow HUD",
         onClick(setting) {
-          // todo
+            ChatLib.command("smarrowhud changegui",true)
         }
     })
 
     
 
-
-
-
-
-
-
-
-/* 
-=======================================================================================================================================================================
-Default gui configs provided by Amaterasu
-=======================================================================================================================================================================
-*/
-    .addTextParagraph({
-        category: "GUI",
-        configName: "textParagraph",
-        title: "Some things might not work",
-        description: "This section came from the Amaterasu example, I left it here in case anyone cared enough to change how their GUI looks. Some stuff might not work.",
-        centered: true 
-        })
-    .addSelection({
-        category: "GUI",
-        configName: "scheme",
-        title: "Change My Scheme!",
-        description: "Select which scheme you want from these presets (needs apply after)",
-        options: ["Default", "Vigil", "nwjn"]
-    })
-    .addButton({
-        category: "GUI",
-        configName: "apply",
-        title: "Apply Changes",
-        description: "Need to click this for window to reload with selected changes",
-        
-        onClick(config) {
-            const currentScheme = schemes[config.settings.scheme]
-            const scheme = JSON.parse(FileLib.read("Amaterasu", currentScheme))
-
-            scheme.Amaterasu.background.color = config.settings.bgColor
-
-            FileLib.write("Amaterasu", currentScheme, JSON.stringify(scheme, null, 4))
-            config
-                .setPos(config.settings.x, config.settings.y)
-                .setSize(config.settings.width, config.settings.height)
-                .setScheme(currentScheme)
-                .apply()
-        }
-    })
-    .addColorPicker({
-        category: "GUI",
-        configName: "bgColor",
-        title: "Change Background Color",
-        description: "Changes the color and alpha of the background",
-        value: [0, 0, 0, 80] 
-    })
-    .addSlider({
-        category: "GUI",
-        configName: "x",
-        title: "Change X",
-        description: "Changes the starting X coordinate of the GUI (in percent)",
-        options: [0, 75],
-        value: 20
-    })
-    .addSlider({
-        category: "GUI",
-        configName: "y",
-        title: "Change Y",
-        description: "Changes the starting Y coordinate of the GUI (in percent)",
-        options: [0, 75],
-        value: 20
-    })
-    .addSlider({
-        category: "GUI",
-        configName: "width",
-        title: "Change Width",
-        description: "Changes the width of the GUI (in percent)",
-        options: [25, 100],
-        value: 60
-    })
-    .addSlider({
-        category: "GUI",
-        configName: "height",
-        title: "Change Height",
-        description: "Changes the height of the GUI (in percent)",
-        options: [25, 100],
-        value: 60
-    })
 
 
 
@@ -198,7 +120,7 @@ Default gui configs provided by Amaterasu
 const SlayerModSettings = new Settings("SlayerMod", defaultConf, "config/ColorScheme.json")
 
     // Here we define a command with aliases to open this [ConfigGui]
-    .setCommand("smopengui")
+    .setCommand("slayermod",["sm"])
 
     // This is how we add our markdowns so the [ConfigGui] knows
     // that we want these to be handled and displayed to the player
@@ -208,13 +130,13 @@ const SlayerModSettings = new Settings("SlayerMod", defaultConf, "config/ColorSc
 
     
 // Change the scheme path depending on the value of the [Selector] config
-const currentScheme = schemes[SlayerModSettings.settings.scheme]
-const scheme = JSON.parse(FileLib.read("Amaterasu", currentScheme))
+// const currentScheme = schemes[SlayerModSettings.settings.scheme]
+// const scheme = JSON.parse(FileLib.read("Amaterasu", currentScheme))
 // Setting the alpha
 // scheme.Amaterasu.background.color = config.settings.bgColor
 
 // Now we save the [currentScheme] with the [alpha] set in the [Slider]
-FileLib.write("Amaterasu", currentScheme, JSON.stringify(scheme, null, 4))
+// FileLib.write("Amaterasu", currentScheme, JSON.stringify(scheme, null, 4))
 
 // Now we set the new position, size and colorScheme for our [ConfigGui]
 SlayerModSettings
